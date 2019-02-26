17a2d58684b6ddec1e1a68c28507d4735e6b15c1
isapsz <42344186+isapsz@users.noreply.github.com>
2019-02-26 11:32:53 -0300

    Update Clinicas.java

--- CRUD/src/bd/dao/Clinicas.java
+++ CRUD/src/bd/dao/Clinicas.java
@@ -39,6 +39,145 @@
 
 	    return clinica;
     }
+	 
+	 public static boolean cadastrado (int codigo) throws Exception
+	 {
+	        boolean retorno = false;
+
+	        try
+	        {
+	            String sql;
+
+	            sql = "SELECT * " +
+	                  "FROM CLINICA " +
+	                  "WHERE CODCLINICA = ?";
+
+	            BDSQLServer.COMANDO.prepareStatement (sql);
+
+	            BDSQLServer.COMANDO.setInt (1, codigo);
+
+	            MeuResultSet resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery ();
+
+	            retorno = resultado.first(); // pode-se usar resultado.last() ou resultado.next() ou resultado.previous() ou resultado.absotule(numeroDaLinha)
+
+	            /* // ou, se preferirmos,
+
+	            String sql;
+
+	            sql = "SELECT COUNT(*) AS QUANTOS " +
+	                  "FROM LIVROS " +
+	                  "WHERE CODIGO = ?";
+
+	            BDSQLServer.COMANDO.prepareStatement (sql);
+
+	            BDSQLServer.COMANDO.setInt (1, codigo);
+
+	            MeuResultSet resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery ();
+
+	            resultado.first();
+
+	            retorno = resultado.getInt("QUANTOS") != 0;
+
+	            */
+	        }
+	        catch (SQLException erro)
+	        {
+	            throw new Exception ("Erro ao procurar clinica");
+	        }
+
+	        return retorno;
+	  }
+	 
+	 public static void incluir (Clinica clinica) throws Exception
+	    {
+	        if (clinica ==null)
+	            throw new Exception ("Cíinica nao fornecido");
+
+	        try
+	        {
+	            String sql;
+
+	            sql = "INSERT INTO CLINICAS " +
+	                  "(CODCLINICA,NOME,CEP,NUMERO,COMPLEMENTO) " +
+	                  "VALUES " +
+	                  "(?,?,?,?,?)";
+
+	            BDSQLServer.COMANDO.prepareStatement (sql);
+
+	            BDSQLServer.COMANDO.setInt    (1, clinica.getCodClinica ());
+	            BDSQLServer.COMANDO.setString (2, clinica.getNome ());
+	            BDSQLServer.COMANDO.setInt  (3, clinica.getCEP ());
+	            BDSQLServer.COMANDO.setInt  (4, clinica.getNumero ());
+	            BDSQLServer.COMANDO.setString  (5, clinica.getComplemento ());
+
+	            BDSQLServer.COMANDO.executeUpdate ();
+	            BDSQLServer.COMANDO.commit        ();
+	        }
+	        catch (SQLException erro)
+	        {
+	            throw new Exception ("Erro ao inserir livro");
+	        }
+	    }
+
+	    public static void excluir (int codigo) throws Exception
+	    {
+	        if (!cadastrado (codigo))
+	            throw new Exception ("Nao cadastrado");
+
+	        try
+	        {
+	            String sql;
+
+	            sql = "DELETE FROM CLINICA " +
+	                  "WHERE CODCLINICA=?";
+
+	            BDSQLServer.COMANDO.prepareStatement (sql);
+
+	            BDSQLServer.COMANDO.setInt (1, codigo);
+
+	            BDSQLServer.COMANDO.executeUpdate ();
+	            BDSQLServer.COMANDO.commit        ();        }
+	        catch (SQLException erro)
+	        {
+	            throw new Exception ("Erro ao excluir clinica");
+	        }
+	    }
+
+	    public static void alterar (Clinica clinica) throws Exception
+	    {
+	        if (clinica==null)
+	            throw new Exception ("Livro nao fornecido");
+
+	        if (!cadastrado (clinica.getCodClinica()))
+	            throw new Exception ("Nao cadastrado");
+
+	        try
+	        {
+	            String sql;
+
+	            sql = "UPDATE CLINICAS " +
+	                  "SET NOME=?, " +
+	                  "CEP=? " + "NUMERO = ?"+
+	                  "COMPLEMENTO = ?"+
+	                  "WHERE CODCLINICA=?";
+
+	            BDSQLServer.COMANDO.prepareStatement (sql);
+
+	            BDSQLServer.COMANDO.setString (1, clinica.getNome ());
+	            BDSQLServer.COMANDO.setInt  (2, clinica.getCEP ());
+	            BDSQLServer.COMANDO.setInt  (3, clinica.getNumero ());
+	            BDSQLServer.COMANDO.setString (4, clinica.getComplemento ());
+	            BDSQLServer.COMANDO.setInt    (5, clinica.getCodClinica ());
+
+	            BDSQLServer.COMANDO.executeUpdate ();
+	            BDSQLServer.COMANDO.commit        ();
+	        }
+	        catch (SQLException erro)
+	        {
+	        	
+	            throw new Exception ("Erro ao atualizar dados de clinica");
+	        }
+	    }
 
     public static MeuResultSet getClinicas() throws Exception
     {
